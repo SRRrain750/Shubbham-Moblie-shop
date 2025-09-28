ca45c1446418826e1bf2dde8820cb9eb6fc2634e
Aabhishek23 <abhishek.parochi.ex21@ggits.net>
2025-09-28 21:47:01 +0530

    all confrigation done with footer and header with tailwind.css

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,26 +2,34 @@ import React from 'react'
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
-
+import { FaWhatsapp } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className='border-t'>
-        <div className='container mx-auto p-4'>
-            <p>© All right reseved 2025</p>
-            <a href=''>
-            <FaFacebook/>
-            </a>
+      <div className="container mx-auto p-4 text-center flex flex-col lg:flex-row lg:justify-between items-center gap-2">
+        <p>© Alll right reseved 2025</p>
 
-            <a href=''>
-            <FaInstagram/>
-            </a>
-            <a href=''>
-            <FaLinkedin/>
-            </a>
-            
+        <div className="flex gap-4 justify-center text-2xl">
+          <a href="#" className="hover:text-primary-100 transition-colors">
+            <FaFacebook />
+          </a>
+
+
+          <a href="#" className="hover:text-pink-500 transition-colors">
+            <FaInstagram />
+          </a>
+          <a href="#" className="hover:text-blue-600 transition-colors">
+            <FaLinkedin />
+          </a>
+          <a href="#" className="hover:text-green-500 transition-colors">
+            <FaWhatsapp />
+          </a>
         </div>
-    </footer>   
+
+
+      </div>
+    </footer>
   )
 }
 
--- frontend/src/route/index.jsx
+++ frontend/src/route/index.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App.jsx";
 import Home from "../pages/Home.jsx";  // 👈 correct path
+import "../index.css";  // go up one folder
+
 
 const router = createBrowserRouter([
   {
