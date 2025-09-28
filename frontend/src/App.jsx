ff533313af309ae5b2c4f6778f227a407dde71e7
Aabhishek23 <abhishek.parochi.ex21@ggits.net>
2025-09-28 17:24:49 +0530

    Save local changes before revert

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
-import Header from './components/Header'
-import Footer from './components/footer'
 
 function App() {
 
-  return (
-    <>
-      <Header />
-      <main>
-        <Outlet />
-      </main>
-      <Footer />
-    </>
+return (
+<main className='text-center text-red-500 font-bold text-5xl'>
+ <Outlet/>
+</main>
   )
 }
 
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,32 +1,35 @@
-import React from 'react'   ;
-import { FaFacebook } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa6";
-import { FaWhatsappSquare } from "react-icons/fa";
+import React from 'react';
+// import { FaFacebook } from "react-icons/fa";
+// import { FaInstagram } from "react-icons/fa";
+// import { FaLinkedin } from "react-icons/fa6";
+// import { FaWhatsappSquare } from "react-icons/fa";
 
 
 const Footer = () => {
-  return (
-    <footer className='border-t'>
-        <div className='container mx-auto p-4'>
-            <p>© All right reseved 2025</p>
-            <a href=''>
-            <FaFacebook/>
-            </a>
+    return (
+        <footer className='border-t'>
+            <div className='container mx-auto p-4'>
+                <p>© Allff right reseved 2025</p>
+                {/* <div className='flex gap-4 text-2xl mt-2'>
+                    <a href=''>
+                        <FaFacebook />
+                    </a>
 
-            <a href=''>
-            <FaInstagram/>
-            </a>
-            <a href=''>
-            <FaLinkedin/>
-            </a>
-            <a href=''>
-            <FaWhatsappSquare/>
-            </a>
+                    <a href=''>
+                        <FaInstagram />
+                    </a>
+                    <a href=''>
+                        <FaLinkedin />
+                    </a>
+                    <a href=''>
+                        <FaWhatsappSquare />
+                    </a>
 
-        </div>
-    </footer>   
-  )
+                </div> */}
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
+Footer
