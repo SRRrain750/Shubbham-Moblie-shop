6a59816257ac246bf6e5c7f6689a52913e2f8741
Aabhishek23 <abhishek.parochi.ex21@ggits.net>
2025-09-30 20:06:18 +0530

    Input in search bar,Serch bar outline color change

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -24,41 +24,41 @@ const Search = () => {
   console.log("search", isSearchPage);
 
   return (
-    <div className='w-full min-w-[320px] lg:min-w-[420px] h-12 rounded-lg border border-neutral-300 overflow-hidden flex items-center px-3 bg-slate-50 '>
-      <button className='cursor-pointer outline-none focus:outline-none p-3 justify-center items-center flex'>
-        <IoSearch size={22} className='text-neutral-600' />
-      </button>
+<div className='w-full min-w-[320px] lg:min-w-[420px] h-12 rounded-lg border border-neutral-300 overflow-hidden flex items-center px-3 bg-slate-50 group focus-within:border-primary-200'>
+  <button className='cursor-pointer outline-none focus:outline-none p-3 justify-center items-center text-neutral-600 group-focus-within:text-primary-200'>
+    <IoSearch size={22} />
+  </button>
 
-      <div>
-        {
-          !isSearchPage ? (
-            //Not on search page
+  <div className='w-full h-full flex items-center px-2'>
+    {
+      !isSearchPage ? (
+        <div onClick={redirectToSearchPage} className='text-neutral-400 w-full h-full flex justify items-center '>
+          <TypeAnimation
+            sequence={[
+              'Search for iPhone models', 1000,
+              'Search for Samsung phones', 1000,
+              'Search for Mobile Accessories', 1000,
+              'Search for Latest Deals', 1000,
+              'Search for Best Sellers', 1000,
+            ]}
+            speed={50}
+            repeat={Infinity}
+          />
+        </div>
+      ) : (
+        <div>
+          <input
+            type="text"
+            placeholder='Search...'
+            autoFocus
+            className='w-full bg-transparent outline-none focus:outline-none text-sm text-neutral-600 placeholder:text-neutral-400'
+          /> 
+        </div>
+      )
+    }
+  </div>
+</div>
 
-            <div onClick={redirectToSearchPage} className='text-neutral-400'>
-              <TypeAnimation
-                sequence={[
-                  // Same substring at the start will only be typed once, initially
-                  // Example for a phone shop
-                  'Search for iPhone models', 1000,
-                  'Search for Samsung phones', 1000,
-                  'Search for Mobile Accessories', 1000,
-                  'Search for Latest Deals', 1000,
-                  'Search for Best Sellers', 1000,
-                ]}
-                speed={50}
-                repeat={Infinity}
-              />
-
-            </div>
-          ) : (
-            // When i was on search page
-            <div>
-            </div>
-          )
-        }
-      </div>
-
-    </div>
   )
 }
 
