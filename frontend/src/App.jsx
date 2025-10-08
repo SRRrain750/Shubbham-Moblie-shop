11d0f072069bc200f91d241be48296ae32080c52
Aabhishek23 <abhishek.parochi.ex21@ggits.net>
2025-10-08 21:37:47 +0530

    refactor: reorganize App component structure and ensure proper import order

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,24 @@
-import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header.jsx'  
 import Footer from './components/Footer.jsx'
 import React from 'react'
 import { Toaster } from 'react-hot-toast' // ✅ import Toaster
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  return(
-    <main className='text'>
-      Mobile-Shop-App
-    </main>
+  return (
+    <>
+      <Header />
+
+      {/* ✅ Toaster for toast popups */}
+      <Toaster position="top-center" reverseOrder={false} />
+
+      <main className='min-h-[75vh]'>
+        <Outlet />
+      </main>
+
+
+
       <Footer/>
     </>
   )
