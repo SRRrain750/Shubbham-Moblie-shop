b23ef25e579f7e1b85f9a31ce22942d114e91699
Aabhishek23 <abhishek.parochi.ex21@ggits.net>
2025-10-11 23:06:27 +0530

    feat: restructure Dashboard layout to include UserMenu and improve content display

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
+import UserMenu from './UserMenu'
 
 const Dashboard = () => {
     return (
         <section className='bg-white'>
-            <div className='container mx-auto p-3 '>
+            <div className='container mx-auto p-3 grid grid-cols-[250px_1fr]'  >
 
-                <div>
-                    {/* Left for menu  */}
-                    <div>
-                        Dashboard Menu
-                    </div>
-                    
-                    {/* Right for Content */}
-                    <div>
-                        Dashboard Content
-                    </div>
+
+                {/* Left for menu  */}
+                <div className='py-4 sticky top-24 overflow-auto-y'>
+                    <UserMenu/>
                 </div>
+
+                {/* Right for Content */}
+                <div className='bg-red-400'>
+                     Content display here
+                </div>
+
             </div>
         </section>
 
